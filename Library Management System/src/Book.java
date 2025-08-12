0b457b6235836c6f1b5c62efc57fa807b2ebe68c
Your Name <your.email@example.com>
2025-08-12 18:24:46 +0530

    Remove unused LocalDate import from Book.java

--- Library Management System/src/Book.java
+++ Library Management System/src/Book.java
@@ -8,6 +8,11 @@ public class Book implements Serializable {
     private String author;
     private String genre;
     private boolean isAvailable;
+    private String isbn;
+    private int publicationYear;
+    private String publisher;
+    private int totalCopies;
+    private int availableCopies;
 
     public Book(String id, String title, String author, String genre) {
         this.id = id;
@@ -15,6 +20,21 @@ public class Book implements Serializable {
         this.author = author;
         this.genre = genre;
         this.isAvailable = true;
+        this.totalCopies = 1;
+        this.availableCopies = 1;
+    }
+    
+    public Book(String id, String title, String author, String genre, String isbn, int publicationYear, String publisher, int totalCopies) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.publisher = publisher;
+        this.totalCopies = totalCopies;
+        this.availableCopies = totalCopies;
+        this.isAvailable = totalCopies > 0;
     }
 
     // Getters and Setters
@@ -40,6 +60,56 @@ public class Book implements Serializable {
 
     public void setAvailable(boolean available) {
         isAvailable = available;
+        if (available) {
+            if (availableCopies < totalCopies) {
+                availableCopies++;
+            }
+        } else {
+            if (availableCopies > 0) {
+                availableCopies--;
+            }
+        }
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+    
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    
+    public String getPublisher() {
+        return publisher;
+    }
+    
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    
+    public int getTotalCopies() {
+        return totalCopies;
+    }
+    
+    public void setTotalCopies(int totalCopies) {
+        this.totalCopies = totalCopies;
+    }
+    
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+    
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+        this.isAvailable = availableCopies > 0;
     }
 
     @Override
@@ -48,8 +118,11 @@ public class Book implements Serializable {
                 "id='" + id + '\'' +
                 ", title='" + title + '\'' +
                 ", author='" + author + '\'' +
+                ", isbn='" + (isbn != null ? isbn : "N/A") + '\'' +
+                ", publisher='" + (publisher != null ? publisher : "N/A") + '\'' +
+                ", publicationYear=" + publicationYear +
                 ", genre='" + genre + '\'' +
                 ", isAvailable=" + isAvailable +
                 '}';
     }
-} 
+}
